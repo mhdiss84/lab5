1902d9426545be8086e53c9492f8a76557db2192
mhdiss84 <mhdis84@my.yorku.ca>
2025-11-26 16:26:28 -0500

    Update TreeNode.java

--- src/main/java/com/example/bst/TreeNode.java
+++ src/main/java/com/example/bst/TreeNode.java
@@ -1,4 +1,27 @@
 package com.example.bst;
 
-public class TreeNode {
+/**
+ * Node class for BST.
+ * 
+ * @param <T> the type of element stored in this node
+ */
+
+public class TreeNode<T> {
+    public T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    /**
+     * Constructs a new node with the specified data and no children.
+     * 
+     * @param data the element to store in this node
+     */
+    
+    public TreeNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
+
+
